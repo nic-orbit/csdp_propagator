fa9f2248d39300c62364573fa38e4ff3b35f6207
nic-orbit <134141799+nic-orbit@users.noreply.github.com>
2024-03-03 16:18:34 +0100

    gg

--- csdp_main.py
+++ csdp_main.py
@@ -24,13 +24,9 @@ my_analyzer.print_statistics()
 
 coverage_table = my_analyzer.get_statistics_dataframe()
 
-<<<<<<< HEAD
 # print only 
 print(coverage_table[['Altitude Section', 'Number of Points', 'Coverage Percentage']])
 
 my_analyzer.write_coverage_percentage_to_file(file_path + "orbit01" )
-=======
-print(coverage_table[['Number of Packages', 'Number of empty Packages', 'Coverage Percentage']])
->>>>>>> 176b5e92472602560b5af9f49e78ac5cd70820bd
 
 my_analyzer.plot_grid_coverage()
